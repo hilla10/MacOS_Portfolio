b02217c53aba097fef562a9ec1f4fcd3ba7f9c1e
hilla10 <hillaman592@gmail.com>
2025-11-26 13:04:44 +0300

    Add Resume and Safari windows with PDF viewing and blog features

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Draggable } from 'gsap/Draggable';
 gsap.registerPlugin(Draggable);
 
 import { Dock, Navbar, Welcome } from '@components';
-import { Terminal } from '@windows';
+import { Resume, Safari, Terminal } from '@windows';
 
 const App = () => {
   return (
@@ -12,6 +12,8 @@ const App = () => {
       <Welcome />
       <Dock />
       <Terminal />
+      <Safari />
+      <Resume />
     </main>
   );
 };
